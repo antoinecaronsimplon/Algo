e751584822650e67af32cad2e8fd6a21dd51016b
AntoineCARON <antoinecaron.simplon@gmail.com>
2017-12-10 15:41:50 +0100

    correction

--- findNumber/js/app.js
+++ findNumber/js/app.js
@@ -1,7 +1,18 @@
 
 function findNumber (number, divice) {
-    //TODO
+	var newArray = [];
+	for (var i=0; i <= number.length; i++){
+	var result = (number[i]%divice);
+	if (result ===0){
+		newArray.push (number[i]);
+	}
+	}
+	return newArray;
 }
+
+// number.filter(function(number)){
+// 	return number%divice ===0
+// }
 
 
 module.exports = {
