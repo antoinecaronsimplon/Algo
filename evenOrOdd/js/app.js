2b403242745b72d3440b9a319e98d8d05fe29718
AntoineCARON <antoinecaron.simplon@gmail.com>
2017-12-10 15:40:15 +0100

    correction

--- evenOrOdd/js/app.js
+++ evenOrOdd/js/app.js
@@ -1,6 +1,14 @@
 
 function evenOrOdd (n) {
-   // TODO
+	// n=parseInt(n);
+	// return ((n & 1)==0)?true:false;
+	// n=parseInt(n);
+	// return (())
+	if((n%2)===0){
+		return 'Even'
+	}else{
+		return 'Odd'
+	}	
 }
 
 
